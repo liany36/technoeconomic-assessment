dfb4ad21aab1be3a24b2829989e23a969184ea38
Yi Lian <liany7666@gmail.com>
2019-07-19 20:57:26 -0700

    complete Expenses--base year

--- api/models/generic-power-only.ts
+++ api/models/generic-power-only.ts
@@ -9,12 +9,35 @@ function GenericPowerOnly(input: InputVarMod) {
     const CapitalCostNEC = input.CapitalCost / input.NetPlantCapacity;
     const AnnualFuelConsumption = FuelConsumptionRate * AnnualHours;
     const AnnualAshDisposal = AnnualFuelConsumption * input.FuelAshConcentration / 100;
+    // Expenses--base year
+    const TotalNonFuelExpenses = input.LaborCost + input.MaintenanceCost + input.InsurancePropertyTax + input.Utilities
+        + input.AshDisposal + input.Management + input.OtherOperatingExpenses;
+    const TotalExpensesIncludingFuel = input.FuelCost * AnnualFuelConsumption + TotalNonFuelExpenses;
+    const FuelCostKwh = AnnualFuelConsumption * input.FuelCost / AnnualGeneration;
+    const LaborCostKwh = input.LaborCost / AnnualGeneration;
+    const MaintenanceCostKwh = input.MaintenanceCost / AnnualGeneration;
+    const InsurancePropertyTaxKwh = input.InsurancePropertyTax / AnnualGeneration;
+    const UtilitiesKwh = input.Utilities / AnnualGeneration;
+    const AshDisposalKwh = input.AshDisposal / AnnualGeneration;
+    const ManagementKwh = input.Management / AnnualGeneration;
+    const OtherOperatingExpensesKwh = input.OtherOperatingExpenses / AnnualGeneration;
+    const TotalNonFuelExpensesKwh = LaborCostKwh + MaintenanceCostKwh + InsurancePropertyTaxKwh
+        + UtilitiesKwh + AshDisposalKwh + ManagementKwh + OtherOperatingExpensesKwh;
+    const TotalExpensesIncludingFuelKwh = FuelCostKwh + TotalNonFuelExpensesKwh;
 
     return {
             'Electrical and Fuel--base year':
                 {'AnnualHours': AnnualHours, 'FuelConsumptionRate': FuelConsumptionRate,
                  'AnnualGeneration': AnnualGeneration, 'CapitalCostNEC': CapitalCostNEC,
-                 'AnnualFuelConsumption': AnnualFuelConsumption, 'AnnualAshDisposal': AnnualAshDisposal}
+                 'AnnualFuelConsumption': AnnualFuelConsumption, 'AnnualAshDisposal': AnnualAshDisposal},
+            'Expenses--base year':
+                {'TotalNonFuelExpenses': TotalNonFuelExpenses, 'TotalExpensesIncludingFuel': TotalExpensesIncludingFuel,
+                 'FuelCostKwh': FuelCostKwh, 'LaborCostKwh': LaborCostKwh, 'MaintenanceCostKwh': MaintenanceCostKwh,
+                 'InsurancePropertyTaxKwh': InsurancePropertyTaxKwh, 'UtilitiesKwh': UtilitiesKwh,
+                 'AshDisposalKwh': AshDisposalKwh, 'ManagementKwh': ManagementKwh,
+                 'OtherOperatingExpensesKwh': OtherOperatingExpensesKwh,
+                 'TotalNonFuelExpensesKwh': TotalNonFuelExpensesKwh,
+                 'TotalExpensesIncludingFuelKwh': TotalExpensesIncludingFuelKwh}
             };
 }
 
