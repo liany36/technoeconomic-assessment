43ef13e8ed7b7a2f945c6f9c76d17e7de8aaada1
Kaiyan Li <kkyli@ucdavis.edu>
2019-07-21 12:53:18 -0700

    add Taxes Financing Income Escalation

--- api/models/tea.model.ts
+++ api/models/tea.model.ts
@@ -1,10 +1,12 @@
 export interface InputVarMod {
     CapitalCost: number;
+    // Electrical and Fuel--base year
     NetPlantCapacity: number;
     CapacityFactor: number;
     NetStationEfficiency: number;
     FuelHeatingValue: number;
     FuelAshConcentration: number;
+    // Expenses--base year
     FuelCost: number;
     LaborCost: number;
     MaintenanceCost: number;
@@ -13,4 +15,21 @@ export interface InputVarMod {
     AshDisposal: number;
     Management: number;
     OtherOperatingExpenses: number;
+    // Taxes
+    FederalTaxRate: number;
+    StateTaxRate: number;
+    ProductionTaxCredit: number;
+    // Financing
+    DebtRatio: number;
+    InterestRateOnDebt: number;
+    EconomicLife: number;
+    CostOfEquity: number;
+    // Income other than energy
+    CapacityPayment: number;
+    InterestRateonDebtReserve: number;
+    // Escalation/Inflation
+    GeneralInflation: number;
+    EscalationFuel: number;
+    EscalationForProductionTaxCredit: number;
+    EscalationOther: number;
 }
