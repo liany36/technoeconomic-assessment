7a01be8320c89364df435641096bad04754ff509
Kaiyan Li <kkyli@ucdavis.edu>
2019-08-13 16:35:01 -0700

    add two routes for hydrogen & gasification-power

--- api/index.ts
+++ api/index.ts
@@ -4,11 +4,13 @@ import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 
 import { GasificationPower } from './models/gasification-power';
+import { GasificationPowerInputMod } from './models/gasification-power.model';
 import { GenericCombinedHeatPower } from './models/generic-combined-heat-power';
 import { GenericCombinedHeatPowerInputMod } from './models/generic-combined-heat-power.model';
 import { GenericPowerOnly } from './models/generic-power-only';
 import { GenericPowerOnlyInputMod } from './models/generic-power-only.model';
 import { Hydrogen } from './models/hydrogen';
+import { HydrogenInputMod } from './models/hydrogen.model';
 
 // tslint:disable-next-line: no-var-requires
 const swaggerDocument = require('../swagger.json');
@@ -33,11 +35,17 @@ app.post('/genericCombinedHeatPower', async (req: any, res: any) => {
   res.status(200).json(result);
 });
 
-// case 'gasification-power':
-//   result = await GasificationPower(req.body);
-//   break;
-// case 'hydrogen':
-//   result = await Hydrogen(req.body);
+app.post('/gasification-power', async (req: any, res: any) => {
+  const params: GasificationPowerInputMod = req.body;
+  const result = await GasificationPower(params);
+  res.status(200).json(result);
+});
+
+app.post('/hydrogen', async (req: any, res: any) => {
+  const params: HydrogenInputMod = req.body;
+  const result = await Hydrogen(params);
+  res.status(200).json(result);
+});
 
 app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
