55bca1c39b2d9353c79f7b47d435f20e21636c87
Kaiyan Li <kkyli@ucdavis.edu>
2019-08-13 16:26:52 -0700

    update routes

--- api/index.ts
+++ api/index.ts
@@ -5,7 +5,9 @@ import swaggerUi from 'swagger-ui-express';
 
 import { GasificationPower } from './models/gasification-power';
 import { GenericCombinedHeatPower } from './models/generic-combined-heat-power';
+import { GenericCombinedHeatPowerInputMod } from './models/generic-combined-heat-power.model';
 import { GenericPowerOnly } from './models/generic-power-only';
+import { GenericPowerOnlyInputMod } from './models/generic-power-only.model';
 import { Hydrogen } from './models/hydrogen';
 
 // tslint:disable-next-line: no-var-requires
@@ -19,29 +21,23 @@ app.use(bodyParser.json());
 
 const port = process.env.PORT || 3000;
 
-app.post('/tearun', async (req: any, res: any) => {
-  // const inputParams: GenericPowerOnlyInputMod = req.body;
-  console.log('\nrequest object = ');
-  console.log(req.body);
-  let result = {};
-  switch (req.body.model) {
-    case 'generic-power-only':
-      result = await GenericPowerOnly(req.body);
-      break;
-    case 'generic-combined-heat-power':
-      result = await GenericCombinedHeatPower(req.body);
-      break;
-    case 'gasification-power':
-      result = await GasificationPower(req.body);
-      break;
-    case 'hydrogen':
-      result = await Hydrogen(req.body);
-  }
-  // const result = await calculate(res.body);
-  console.log('result object = ');
-  console.log(result);
+app.post('/genericPowerOnly', async (req: any, res: any) => {
+  const params: GenericPowerOnlyInputMod = req.body;
+  const result = await GenericPowerOnly(params);
   res.status(200).json(result);
 });
+
+app.post('/genericCombinedHeatPower', async (req: any, res: any) => {
+  const params: GenericCombinedHeatPowerInputMod = req.body;
+  const result = await GenericCombinedHeatPower(params);
+  res.status(200).json(result);
+});
+
+// case 'gasification-power':
+//   result = await GasificationPower(req.body);
+//   break;
+// case 'hydrogen':
+//   result = await Hydrogen(req.body);
 
 app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
