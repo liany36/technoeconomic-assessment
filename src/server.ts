9e74a575d344f479250f96cf4fdf74f7294e20c0
Laura Holstege <laholstege@ucdavis.edu>
2019-09-05 13:18:53 -0700

    rename index to server

---
+++ src/server.ts
@@ -0,0 +1,63 @@
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+
+import { GasificationPower } from './models/gasification-power';
+import { GenericCombinedHeatPower } from './models/generic-combined-heat-power';
+import { GenericPowerOnly } from './models/generic-power-only';
+import { Hydrogen } from './models/hydrogen';
+import {
+  InputModCHP,
+  InputModGP,
+  InputModGPO,
+  InputModHydrogen
+} from './models/input.model';
+
+// tslint:disable-next-line: no-var-requires
+const swaggerDocument = require('../swagger.json');
+
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
+const port = process.env.PORT || 3000;
+
+app.post('/genericPowerOnly', async (req: any, res: any) => {
+  const params: InputModGPO = req.body;
+  const result = await GenericPowerOnly(params);
+  res.status(200).json(result);
+});
+
+app.post('/genericCombinedHeatPower', async (req: any, res: any) => {
+  const params: InputModCHP = req.body;
+  const result = await GenericCombinedHeatPower(params);
+  res.status(200).json(result);
+});
+
+app.post('/gasificationPower', async (req: any, res: any) => {
+  const params: InputModGP = req.body;
+  const result = await GasificationPower(params);
+  res.status(200).json(result);
+});
+
+app.post('/hydrogen', async (req: any, res: any) => {
+  const params: InputModHydrogen = req.body;
+  const result = await Hydrogen(params);
+  res.status(200).json(result);
+});
+
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
