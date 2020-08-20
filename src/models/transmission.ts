3ac57e561a422734823fb17e4dd757aac8a0fcb9
Kaiyan Li <kkyli@ucdavis.edu>
2020-08-19 22:17:41 -0700

    345 kV Double Circuit

--- src/models/transmission.ts
+++ src/models/transmission.ts
@@ -214,6 +214,52 @@ export function TransmissionCost(input: InputModTransimission) {
       }
       ROWwidth = 175;
       break;
+      case '345 kV Double Circuit': // -----------------------------------------------
+      InitialCost = 2077000;
+      InitialCost = InitialCost * (1 + InflationRate);
+      switch (input.ConductorType) {
+        case 'ACSR':
+          Multiplier.Conductor = 1;
+          LineLossUnitMile = 0.3212;
+          break;
+        case 'ACSS':
+          Multiplier.Conductor = 1.08;
+          LineLossUnitMile = 0.8018;
+          break;
+        case 'HTLS':
+          Multiplier.Conductor = 3.6;
+          LineLossUnitMile = 0.7918;
+          break;
+      }
+      switch (input.Structure) {
+        case 'Lattice':
+          Multiplier.Structure = 1;
+          break;
+        case 'Tubular Steel':
+          Multiplier.Structure = 1.3;
+          break;
+      }
+      switch (input.LengthCategory) {
+        case '> 10 miles':
+          Multiplier.Length = 1;
+          break;
+        case '3-10 miles':
+          Multiplier.Length = 1.2;
+          break;
+        case '< 3 miles':
+          Multiplier.Length = 1.5;
+          break;
+      }
+      switch (input.NewOrReconductor) {
+        case 'New':
+          Multiplier.NewOrReconductor = 1;
+          break;
+        case 'Re-conductor':
+          Multiplier.NewOrReconductor = 0.55;
+          break;
+      }
+      ROWwidth = 200;
+      break;
   }
   TotalMiles =
     input.Miles.Forested +
