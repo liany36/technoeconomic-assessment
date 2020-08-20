1ad3dc4e54da4aad82b4307cc1b7ea23f78db458
Kaiyan Li <kkyli@ucdavis.edu>
2020-08-19 22:20:52 -0700

    500 kV Single Circuit

--- src/models/transmission.ts
+++ src/models/transmission.ts
@@ -214,7 +214,7 @@ export function TransmissionCost(input: InputModTransimission) {
       }
       ROWwidth = 175;
       break;
-      case '345 kV Double Circuit': // -----------------------------------------------
+    case '345 kV Double Circuit': // -----------------------------------------------
       InitialCost = 2077000;
       InitialCost = InitialCost * (1 + InflationRate);
       switch (input.ConductorType) {
@@ -237,6 +237,52 @@ export function TransmissionCost(input: InputModTransimission) {
           break;
         case 'Tubular Steel':
           Multiplier.Structure = 1.3;
+          break;
+      }
+      switch (input.LengthCategory) {
+        case '> 10 miles':
+          Multiplier.Length = 1;
+          break;
+        case '3-10 miles':
+          Multiplier.Length = 1.2;
+          break;
+        case '< 3 miles':
+          Multiplier.Length = 1.5;
+          break;
+      }
+      switch (input.NewOrReconductor) {
+        case 'New':
+          Multiplier.NewOrReconductor = 1;
+          break;
+        case 'Re-conductor':
+          Multiplier.NewOrReconductor = 0.55;
+          break;
+      }
+      ROWwidth = 200;
+      break;
+    case '500 kV Single Circuit': // -----------------------------------------------
+      InitialCost = 1854000;
+      InitialCost = InitialCost * (1 + InflationRate);
+      switch (input.ConductorType) {
+        case 'ACSR':
+          Multiplier.Conductor = 1;
+          LineLossUnitMile = 0.1079;
+          break;
+        case 'ACSS':
+          Multiplier.Conductor = 1.08;
+          LineLossUnitMile = 0.284;
+          break;
+        case 'HTLS':
+          Multiplier.Conductor = 3.6;
+          LineLossUnitMile = 0.2968;
+          break;
+      }
+      switch (input.Structure) {
+        case 'Lattice':
+          Multiplier.Structure = 1;
+          break;
+        case 'Tubular Steel':
+          Multiplier.Structure = 1.5;
           break;
       }
       switch (input.LengthCategory) {
